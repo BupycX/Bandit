2d81fdb02b3867f8259cd484d5d92a63afa3ece3
Bupyc_X <Bupyc3@rambler.ru>
2016-07-27 20:30:48 +0400

    Доработка

--- Bandit.js
+++ Bandit.js
@@ -4,4 +4,17 @@ function (richag)
 var a = (1, 2, 3, 4, 5);
 var b = (1, 2, 3, 4, 5);
 var c = (1, 2, 3, 4, 5);
+
+	var ran_a = (Math.random() * a)
+	{
+	return ran_a
+	}
+	var ran_b = (Math.random() * b)
+	{
+	return ran_b
+	}
+	var ran_c = (Math.random() * c)
+	{
+	return ran_c
+	}
 }
